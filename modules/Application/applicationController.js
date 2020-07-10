016168768870cd198684454a53f5d1b1a4955bb8
Manikandan Thiruvenkadaraj <manikandant@karyatech.com>
2020-07-10 22:00:06 +0530

    BUg fixing

--- modules/Application/applicationController.js
+++ modules/Application/applicationController.js
@@ -8,10 +8,10 @@ const multer = require('multer')
 const session = require('express-session');
 const { Validator } = require('node-input-validator');
 const mongoose = require('mongoose')
-
+const Category = require('../Categories/categoriesModel');
 const Application = require('./applicationModel')
 const ApplicationNotification = require('../Notification/applicationNotificationModel')
-app.use(session({secret: 'ssshhhhh', saveUninitialized: true, resave: true}));
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
 router.use(cors())
 
 process.env.SECRET_KEY = 'secret'
@@ -48,7 +48,7 @@ router.post('/create', upload.single('file'), (req, res) => {
         if (file && file.filename)
           req.body.requirement_documents = file.filename;
 
-        if(sessionStorage.token){
+        if (sessionStorage.token) {
           decoded = jwt.verify(sessionStorage.token, process.env.SECRET_KEY)
           req.body.userID = decoded._id;
         }
@@ -99,28 +99,28 @@ router.post('/create', upload.single('file'), (req, res) => {
         else {
           Application.create(req.body)
             .then(response => {
-              if(response){
+              if (response) {
                 let notificationData = {
                   applicationID: response.id,
                   userID: req.body.userID,
                   createdAt: new Date().toISOString()
                 }
                 ApplicationNotification.create(notificationData)
-                .then(response1 => {
-                  res.status(200).json({ success: response })
-                })
-                .catch(err => {
-                  var message = '';
-                  if (err.message) {
-                    message = err.message;
-                  }
-                  else {
-                    message = err;
-                  }
-                  return res.status(400).send({
-                    message: message
-                  });
-                })
+                  .then(response1 => {
+                    res.status(200).json({ success: response })
+                  })
+                  .catch(err => {
+                    var message = '';
+                    if (err.message) {
+                      message = err.message;
+                    }
+                    else {
+                      message = err;
+                    }
+                    return res.status(400).send({
+                      message: message
+                    });
+                  })
               }
             })
             .catch(err => {
@@ -243,9 +243,156 @@ router.get('/get', (req, res) => {
     })
 })
 
+router.post('/getFilter', (req, res) => {
+  Application.find({ 'status.status': req.body.status })
+    .then(response => {
+      if (response) {
+        res.status(200).json(response)
+      } else {
+        res.send('Application not found')
+      }
+    })
+    .catch(err => {
+      var message = '';
+      if (err.message) {
+        message = err.message;
+      }
+      else {
+        message = err;
+      }
+      return res.status(400).send({
+        message: message
+      });
+    })
+})
+
+router.post('/getFilterCategory', (req, res) => {
+  Application.find({ [req.body.property]: req.body.value })
+    .then(response => {
+      if (response) {
+        res.status(200).json(response)
+      } else {
+        res.send('Application not found')
+      }
+    })
+    .catch(err => {
+      var message = '';
+      if (err.message) {
+        message = err.message;
+      }
+      else {
+        message = err;
+      }
+      return res.status(400).send({
+        message: message
+      });
+    })
+})
+
+
+router.get('/getCounts', async (req, res) => {
+  var categories = () => {
+    return new Promise((resolve, reject) => {
+      Category.find({ parent_category: null })
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var subcategories = () => {
+    return new Promise((resolve, reject) => {
+      Category.aggregate([
+        { $match: { parent_category: { $exists: true, $ne: null } } }
+      ])
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var applications = () => {
+    return new Promise((resolve, reject) => {
+      Application.find({})
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var pending = () => {
+    return new Promise((resolve, reject) => {
+      Application.find({ 'status.status': 'Pending' })
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var completed = () => {
+    return new Promise((resolve, reject) => {
+      Application.find({ 'status.status': 'Completed' })
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var rejected = () => {
+    return new Promise((resolve, reject) => {
+      Application.find({ 'status.status': 'Rejected' })
+        .exec(function (err, data) {
+          err
+            ? reject(err)
+            : resolve(data.length);
+        });
+    });
+  };
+  var categoriesCount = await categories();
+  var subcategoriesCount = await subcategories();
+  var applicationsCount = await applications();
+  var pendingCount = await pending();
+  var completedCount = await completed();
+  var rejectedCount = await rejected();
+  var params = {
+    categoriesCount: categoriesCount,
+    subcategoriesCount: subcategoriesCount,
+    applicationsCount: applicationsCount,
+    pendingCount: pendingCount,
+    completedCount: completedCount,
+    rejectedCount: rejectedCount
+  }
+  res.json(params);
+  // Application.find({})
+  //   .then(response => {
+  //     if (response) {
+  //       res.status(200).json(response)
+  //     } else {
+  //       res.send('Application not found')
+  //     }
+  //   })
+  //   .catch(err => {
+  //     var message = '';
+  //     if (err.message) {
+  //       message = err.message;
+  //     }
+  //     else {
+  //       message = err;
+  //     }
+  //     return res.status(400).send({
+  //       message: message
+  //     });
+  //   })
+})
+
 router.get('/view', (req, res) => {
   try {
-    Application.find({_id: req.query.applicationId})
+    Application.findOne({ _id: req.query.applicationid })
       .then(response => {
         res.status(200).json(response);
       })
